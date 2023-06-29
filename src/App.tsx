992d82b8db8801cd830168178743d6f8cc007c40
jeongminsang <zjzj@hanmail.net>
2023-06-30 05:38:41 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Survey from './pages/Survey';
+import Result from './pages/Result';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/survey' element={<Survey />} />
+        <Route path='/result/:MbtiName' element={<Result />} />
       </Routes>
     </div>
   );
